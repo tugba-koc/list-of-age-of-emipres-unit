e953ffc2b715140962c50501d324a90c6eab5857
tugba-koc <koctug@itu.edu.tr>
2022-05-23 22:59:55 +0300

    feat : add router structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home';
+import Units from './pages/Units';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/units">Units Page</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="units" element={<Units />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Units.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Units() {
+  return (
+    <div>Units</div>
+  )
+}
+
+export default Units
